b4049d0016af2d6e7cdc24852d9be0684e1017f6
RandomUser.js <arthurbarrosramos7@gmail.com>
2024-01-29 11:58:48 -0300

    yep

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto)
 {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, "Brasilian Portuguese Female", {rate:1.2});
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2});
 }
 
 function exibirMensagemInicial()
